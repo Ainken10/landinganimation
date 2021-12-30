02b48b9782aec59c746db5ba33f728c7625cba26
Suszta Domos <domos.suszta@hotmail.com>
2021-12-30 14:18:18 +0100

    landingInit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,97 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
 
 export default function Home() {
+  const animation = useAnimation();
+  const animationText = useAnimation();
+  const animationText2 = useAnimation();
+  const animationHeader = useAnimation();
+
+  useEffect(async () => {
+    await animation.start({ y: 0, opacity: 1 });
+    await animationText.start({ opacity: 1, x: 0 });
+    await animationHeader.start({ opacity: 1 });
+
+    animation.start({ y: 100, opacity: 1 });
+    animationText.start({ y: 100 });
+      //  await animation.start({ height: '550px' });
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex flex-col min-h-screen">
+      <motion.div
+        className="flex absolute w-full h-28 shadow-md"
+        transition={{ duration: 0.2 }}
+        initial={{ opacity: 0 }}
+        animate={animationHeader}
+      >
+        
+        <motion.div className="w-1/5 h-full bg-black flex items-center justify-center">
+          <p className="font-Poppins text-3xl text-white  font-semibold">
+            Vans.
+          </p>
+        </motion.div>
+        <motion.div className="w-3/5 h-full font-semibold text-lg bg-white flex items-center justify-center space-x-14">
+          <p>Design</p>
+          <p>Strategy</p>
+          <p>Cases</p>
+          <p>about</p>
+          <p>Contact</p>
+        </motion.div>
+        <motion.div className="w-1/5 h-full bg-white flex items-center justify-center">
+          <p className="font-Poppins text-xl  border-b-2  border-black pb-2  font-semibold">
+            Let's work together
+          </p>
+        </motion.div>
+      </motion.div>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <div className=" w-full flex flex-wrap relative">
+        <motion.img
+          className="w-full sm:w-1/4 h-72"
+          transition={{ duration: 0.2 }}
+          initial={{ opacity: 0, y: -100 }}
+          animate={animation}
+          src="vans/1.jpg"
+        />
+        <motion.img
+          className="w-full sm:w-1/4 h-72"
+          transition={{ duration: 0.4 }}
+          initial={{ opacity: 0, y: -100 }}
+          animate={animation}
+          src="vans/2.jpg"
+        />
+        <motion.img
+          className="w-full sm:w-1/4 h-72"
+          transition={{ duration: 0.6 }}
+          initial={{ opacity: 0, y: -100 }}
+          animate={animation}
+          src="vans/3.jpg"
+        />
+        <motion.img
+          className="w-full sm:w-1/4 h-72"
+          transition={{ duration: 0.8 }}
+          initial={{ opacity: 0, y: -100 }}
+          animate={animation}
+          src="vans/4.jpg"
+        />
+      </div>
+      <motion.h1
+        className="font-bold text-8xl font-Poppins"
+        transition={{ duration: 0.6 }}
+        initial={{ opacity: 0, x: -100 }}
+        animate={animationText}
+      >
+        Vans are conquering the world.
+      </motion.h1>
+      <motion.h1
+        className="font-semibold text-7xl font-Poppins text-right"
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 3.5 }}
+        animate={{ opacity: 1, x: 0, y: 10 }}
+      >
+        Get one now!
+      </motion.h1>
     </div>
-  )
+  );
 }
